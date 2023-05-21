3adffeeb6ce2b354f4fc3e75a24a986c59eea6cf
1zumisawashun <shun.1zumisawa@gmail.com>
2023-05-21 17:49:06 +0900

    feat: :sparkles: 案内の画像を追加する

--- src/components/templates/Home.tsx
+++ src/components/templates/Home.tsx
@@ -1,6 +1,10 @@
 import { styled } from '@mui/material/styles'
 import { BaseText, BaseTitle } from '@/functions/themes'
 import { PanelCard } from '@/components/uis'
+import image_contact_complete from '@/assets/image_contact_complete.png'
+import image_contact_confirm from '@/assets/image_contact_confirm.png'
+import image_contact_form from '@/assets/image_contact_form.png'
+import image_slack from '@/assets/image_slack.png'
 
 const GapWrapper = styled('div')`
   display: grid;
@@ -25,7 +29,7 @@ export const Home: React.FC = () => {
       <BaseTitle>ホーム画面</BaseTitle>
 
       <PanelCard title="プログラム">
-        <BaseText>以下参照してください。</BaseText>
+        <BaseText>以下リンクを参照してください。</BaseText>
         <UnorderedList>
           <li>
             <a href="https://github.com/1zumisawashun/contact-form-react">
@@ -36,7 +40,7 @@ export const Home: React.FC = () => {
       </PanelCard>
 
       <PanelCard title="提出したプログラムの動作の確認手順">
-        <BaseText>以下参照してください。</BaseText>
+        <BaseText>以下リンクを参照してください。</BaseText>
         <UnorderedList>
           <li>
             <a href="https://github.com/1zumisawashun/contact-form-react#installation">
@@ -44,6 +48,34 @@ export const Home: React.FC = () => {
             </a>
           </li>
         </UnorderedList>
+
+        <BaseText>
+          インストール完了後ヘッダーからお問い合わせフォームを確認することができます。以下はお問い合わせの導線画像になります。
+        </BaseText>
+        <img
+          src={image_contact_form}
+          alt="image_contact_form"
+          width="100%"
+          height="auto"
+        />
+        <img
+          src={image_contact_confirm}
+          alt="image_contact_form"
+          width="100%"
+          height="auto"
+        />
+        <img
+          src={image_contact_complete}
+          alt="image_contact_form"
+          width="100%"
+          height="auto"
+        />
+        <img
+          src={image_slack}
+          alt="image_contact_form"
+          width="100%"
+          height="auto"
+        />
       </PanelCard>
 
       <PanelCard title="提出物に対して、コメント">
@@ -58,7 +90,8 @@ export const Home: React.FC = () => {
             v6.4から導入されたScrollRestorationを使ってMPAと同じ挙動にしUXの向上を意識しました。（SPAの場合、画面遷移時スクロール位置がそのままになってしまうため）
           </li>
           <li>
-            送信データを標準出力するだけでなく、slack通知を導入してログを取れるように工夫しました。
+            送信データを標準出力するだけでなく、slack通知を導入してログを取れるように工夫しました。（slack通知の確認をする際はwebhook
+            url生成が必要になるので「slack通知を導入する際に書いた記事」を参照してください）
           </li>
           <li>
             カタログ画面を作成しコンポーネントの可視化できる環境を整えました。（今回の実装においてStroybookの導入はオーバーヘッドになると考えたため）
